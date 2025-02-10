1db27e40d8b1323d53f5ab3ed2c1fcd478bd5b4b
Dima <dimtar009@gmail.com>
2025-02-10 13:46:38 +0300

    crud

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from '@prisma/client';
 import { UserDto } from 'src/dto/user.dto';
@@ -7,8 +16,28 @@ import { UserDto } from 'src/dto/user.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Get('get')
+  findOne(@Query('id') id: string) {
+    return this.authService.findOne(Number(id));
+  }
+
+  @Get('getAll')
+  findAll() {
+    return this.authService.findAll();
+  }
+
   @Post('register')
-  register(@Body() userDto : UserDto) {
-    return this.authService.register(userDto);
+  register(@Body() userDto: Pick<UserDto, 'name'>) {
+    return this.authService.register(userDto.name);
+  }
+
+  @Delete('delete')
+  delete(@Body() UserDto: Pick<UserDto, 'id'>) {
+    return this.authService.delete(UserDto.id);
+  }
+
+  @Put('update')
+  update(@Body() UserDto: UserDto) {
+    return this.authService.update(UserDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,78 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient, User } from '@prisma/client';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { UserDto } from 'src/dto/user.dto';
 
 @Injectable()
 export class AuthService {
   prisma = new PrismaClient();
 
-  async register(userDto: UserDto) {
+  async findOne(findId: number) {
+    console.log(findId);
+
+    const user = await this.prisma.user.findFirst({
+      where: {
+        id: findId,
+      },
+    });
+
+    if (!user) {
+      throw new BadRequestException('ай-яй-яй');
+    }
+
+    return user.name;
+  }
+
+  async findAll() {
+    return await this.prisma.user.findMany();
+  }
+
+  async register(registerName: string) {
     const user = await this.prisma.user.create({
+      data: {
+        name: registerName,
+      },
+    });
+    return user;
+  }
+
+  async delete(deleteId: number) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        id: deleteId,
+      },
+    });
+
+    if (!user) {
+      throw new BadRequestException('ай-яй-яй');
+    }
+
+    const answer = await this.prisma.user.delete({
+      where: {
+        id: deleteId,
+      },
+    });
+    return answer;
+  }
+
+  async update(userDto: UserDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        id: userDto.id,
+      },
+    });
+
+    if (!user) {
+      throw new BadRequestException('ай-яй-яй');
+    }
+
+    const updatedUser = this.prisma.user.update({
+      where: {
+        id: userDto.id,
+      },
       data: {
         name: userDto.name,
       },
     });
-    return user;
+    return updatedUser;
   }
 }
